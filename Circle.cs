ea860b73a031ad0c2b2efa8db9939d34add9fbf4
Anatoliy-Bezkrovnyi <bezkrovnyi1@gmail.com>
2019-05-28 08:09:32 +0300

    New Class Square was added

---
+++ Circle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIgureSquare
+{
+    public class Circle : Figure
+    {
+        public static double radius;
+        public const double pi = 3.14;
+        public override double CountFigureSquare(double radius)
+        {
+            return pi* radius * radius;
+        }
+    }
+}
---
+++ FIgure.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIgureSquare
+{
+    public abstract class Figure
+    {
+        public double parameter;
+        public abstract double CountFigureSquare(double parameter);
+        public void GetParameterFromKeyboard()
+        {
+            while (!Double.TryParse(Console.ReadLine(), out double parsedInput))
+            {
+                Console.WriteLine("Неверный ввод");
+                Console.ReadLine();
+            }
+            parameter = Double.Parse(Console.ReadLine());
+        }
+        
+    }
+}
