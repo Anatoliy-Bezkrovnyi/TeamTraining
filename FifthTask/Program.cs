d2a7bdf00f93f120513b05a792c94fdc9d76aab8
Anatolii Bezkrovnyi <bezkrovnyi1@gmail.com>
2019-07-17 15:36:23 +0300

    Added getting time needed to complete all tasks.

--- FifthTask/Program.cs
+++ FifthTask/Program.cs
@@ -8,16 +8,18 @@ namespace FifthTask
     {
         static void Main(string[] args)
         {
-           
 
+            Request request = new Request();
             List<Task> taskList = new List<Task>();
+
+
             Input.AddNewTask(ref taskList);
 
             foreach (Task task in taskList)
             {
                 Output.PrintTaskDetails(task);
             }
-
+            request.AmountOfTimeNeededToSolveAllTasks(taskList);
 
             
         }
--- FifthTask/Request.cs
+++ FifthTask/Request.cs
@@ -1,18 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace FifthTask
 {
     public class Request
     {
-        public void AmountOfTimeNeededToSolveAllTasks()
+        public void AmountOfTimeNeededToSolveAllTasks(List<Task> taskList)
         {
 
+            int timeSum = 0;
+            foreach (Task task in taskList)
+            {
+                timeSum += task.Time;
+            }
+
+            //int t = taskList.Sum(x => x.Time);
+
+            Console.WriteLine($"Total amount of time to solve all tasks is {timeSum} hours.");
         }
 
         public void ListofTasksAccordingToPriority()
         {
+            Console.WriteLine("Please enter tasks priority to find all corresponding tasks, use 'High', 'Medium', 'Low'.");
+            String enteredPriority = Console.ReadLine();
+
+            int attempts = 3;
+
+            while (attempts != 0)
+            {
+                switch (enteredPriority)
+                {
+                    case "High":
+
+                        break;
+                    case "Medium":
+
+                        break;
+                    case "Low":
+
+                        break;
+                    default:
+                        Console.WriteLine("Invalid input, please use ONLY 'High', 'Medium', 'Low' options");
+                        attempts--;
+                        break;
+                }
+            }
+            
 
         }
 
