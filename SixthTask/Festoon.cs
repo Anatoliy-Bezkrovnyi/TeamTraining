829e81c2e998e8a742a2e9727e33aae6df2bad39
Anatolii Bezkrovnyi <bezkrovnyi1@gmail.com>
2019-07-23 17:00:05 +0300

    Started work on sixth task.

---
+++ SixthTask/Festoon.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SixthTask
+{
+    public abstract class Festoon
+    {
+        public abstract void CreateFestoon();
+    }
+}
---
+++ SixthTask/LightBulb.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SixthTask
+{
+    public class LightBulb
+    {
+    }
+}
